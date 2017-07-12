03dfdbbfa39d73b2304ac7d9d4d01ddcabdec83f
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-07-12 16:09:05 -0400

    updated placeholders to underscore text instead of divs

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-console.log('linked')
-// set up categories, each with an array of 3 words - gold
-// var movies = ['jaws', 'braveheart', 'titanic'] - gold
-// var songs = ['vogue', 'thriller', 'uptown funk'] - gold
-// var books = ['crime and punishment', 'the bible', 'the great gatsby'] - gold
-// user click category to select which category to use - gold
-// break word into letters
-// create placeholders for each item - div with class (letter)
-    // spaces? if space (or not letter a-z)
-// user click on letter (divs with ids corresponding to (letters or numbers?))
-    // mark letter used
-// check if letter clicked matches letters
-    // if yes
-      // store letter
-      // show letter - insert in the div?
-    // if no
-      // add hangman's body part
-      // count wrong
-        // if wrong count === 6 (total parts), game over
-// continue until wrong count, or user click - solve.
-  // if solve check if correct letters stored equals correct letters in word
 var selWord = 'forrest gump'
 var wordArray
 
@@ -26,37 +5,96 @@ function setPlaceholder () {
   wordArray = selWord.split('')
   wordArray.forEach(wordDivs)
   console.log(wordArray)
-  console.log(wordArray.length)
 }
+
 
 function wordDivs (wordArray) {
   for (let i = 0; i < wordArray.length; i++) {
       if (wordArray[i] === ' ') {
-        $('<div></div>').attr('class', 'space').appendTo('.word')
+        $('.word').append(' ')
       }
-      else $('<div></div>').attr('class', 'letter').appendTo('.word')
+      else $('.word').append('_')
   }
 }
 
-setPlaceholder();
+setPlaceholder()
 
 var alph = $('.alph')
 var alphPick
 var alphList = []
+var rightPicks = []
+var wrongPicks = []
 
 alph.on('click', alphHover)
 alph.on('click', alphSelect)
 alph.on('click', addToList)
+alph.on('click', checkPick)
+alph.on('click', showMan)
+alph.on('click', display)
+alph.on('click', resetDivs)
 
 function alphHover () { $(this).css('opacity', '0.5') }
 function alphSelect () { alphPick = $(this).attr('id') }
 function addToList () { alphList.push(alphPick) }
 
 function checkPick () {
-  forEach(wordArray)
+  if (wordArray.includes(alphPick)) {
+    console.log(true)
+    rightPicks.push(alphPick)
+  }
+  else {
+    console.log(false)
+    wrongPicks.push(alphPick)
+  }
 }
 
-// check if alphPick is in selWord
+function showMan () {
+  if (wrongPicks.length === 1) {
+    $('.head').removeClass('hidden')
+  }
+  else if (wrongPicks.length === 2) {
+    $('.torso').removeClass('hidden')
+  }
+  else if (wrongPicks.length === 3) {
+    $('.left-arm').removeClass('hidden')
+  }
+  else if (wrongPicks.length === 4) {
+    $('.right-arm').removeClass('hidden')
+  }
+  else if (wrongPicks.length === 5) {
+    $('.left-leg').removeClass('hidden')
+  }
+  else if (wrongPicks.length === 6) {
+    $('.right-leg').removeClass('hidden')
+    // show answer - or make click to see?
+  }
+}
 
-// $('button').on('click', grow)
-// function grow() {$('.torso').animate({height: '+=120px'}, 2000)}
+var displayArray
+
+function display () {
+  displayArray = wordArray.map((letter) => {
+  if (alphList.includes(letter)) {
+    return letter
+  } else if (alphList.includes(' ')) {
+    return ' '
+  } else {
+    return '_'
+    }
+  })
+  console.log(displayArray)
+}
+
+function resetDivs () {
+  displayArray.forEach(redoDivs)
+  console.log('divs reset')
+}
+
+function redoDivs (displayArray) {
+  for (let i = 0; i < displayArray.length; i++) {
+      if (displayArray[i] === '_') {
+        $('<div>_</div>').attr('class', 'space').appendTo('.word')
+      }
+      else $('<div> </div>').attr('class', 'letter').text(displayArray[i]).appendTo('.word')
+  }
+}
