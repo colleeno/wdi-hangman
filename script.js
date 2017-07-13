495d747bd6ea5912526ddd8770a1570b26e35ba5
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-07-13 13:58:50 -0400

    game working on cat pick to start

--- script.js
+++ script.js
@@ -41,89 +41,89 @@ function setPlaceholder () {
 
 function startGame () {
 
-var alph = $('.alph')
-var alphPick
-var allPicks = []
-var rightPicks = []
-var wrongPicks = []
-var showRight
-var displayWord
-var getAnswer = $('#answer')
-var endHideElem = $('h2, .word, ul, footer')
+  var alph = $('.alph')
+  var alphPick
+  var allPicks = []
+  var rightPicks = []
+  var wrongPicks = []
+  var showRight
+  var displayWord
+  var getAnswer = $('#answer')
+  var endHideElem = $('h2, .word, ul, footer')
 
-alph.on('click', alphSelect)
-alph.on('click', alphUsed)
-alph.on('click', addToList)
-alph.on('click', checkPick)
-alph.on('click', reveal)
-alph.on('click', showMan)
-getAnswer.on('click', showAnswer)
-getAnswer.on('click', function () { getAnswer.css({'opacity': '0', 'cursor': 'default'}) })
+  alph.on('click', alphSelect)
+  alph.on('click', alphUsed)
+  alph.on('click', addToList)
+  alph.on('click', checkPick)
+  alph.on('click', reveal)
+  alph.on('click', showMan)
+  getAnswer.on('click', showAnswer)
+  getAnswer.on('click', function () { getAnswer.css({'opacity': '0', 'cursor': 'default'}) })
 
-/* mark and store letters selected */
-function alphUsed () { $(this).css({'opacity': '0.2', 'cursor': 'auto'}) }
-function alphSelect () { alphPick = $(this).attr('id') }
-function addToList () { allPicks.push(alphPick) }
+  /* mark and store letters selected */
+  function alphUsed () { $(this).css({'opacity': '0.2', 'cursor': 'auto'}) }
+  function alphSelect () { alphPick = $(this).attr('id') }
+  function addToList () { allPicks.push(alphPick) }
 
-/* check if pick is right or wrong and track in array */
-function checkPick () {
-  if (wordArray.includes(alphPick)) {
-    rightPicks.push(alphPick)
-  } else {
-    wrongPicks.push(alphPick)
-    $('.stand-base').append(alphPick)
+  /* check if pick is right or wrong and track in array */
+  function checkPick () {
+    if (wordArray.includes(alphPick)) {
+      rightPicks.push(alphPick)
+    } else {
+      wrongPicks.push(alphPick)
+      $('.stand-base').append(alphPick)
+    }
   }
-}
 
-/* reveal right letters on board */
-function reveal () {
-  showRight = wordArray.map((letter) => {
-    if (allPicks.includes(letter)) {
-      return letter
-    } else if (letter === ' ') {
-      return ' '
-    } else return '_'
-  })
-  displayWord = showRight.join('')
-  wordCanvas.text(displayWord)
+  /* reveal right letters on board */
+  function reveal () {
+    showRight = wordArray.map((letter) => {
+      if (allPicks.includes(letter)) {
+        return letter
+      } else if (letter === ' ') {
+        return ' '
+      } else return '_'
+    })
+    displayWord = showRight.join('')
+    wordCanvas.text(displayWord)
 
-/* display win when all letters guessed */
-  if (displayWord === wordPlay) {
-    console.log('won')
-    endHideElem.css('opacity', '0.5')
-    $('aside').css('opacity', '0.5')
-    $('#result').text('YOU WIN!')
-    $('#play-again').text('play again')
+  /* display win when all letters guessed */
+    if (displayWord === wordPlay) {
+      console.log('won')
+      endHideElem.css('opacity', '0.5')
+      $('aside').css('opacity', '0.5')
+      $('#result').text('YOU WIN!')
+      $('#play-again').text('play again')
+    }
   }
-}
 
-/* wrong answers to reveal hangman */
-function showMan () {
-  if (wrongPicks.length === 1) {
-    $('.head').removeClass('hidden')
-  } else if (wrongPicks.length === 2) {
-    $('.torso').removeClass('hidden')
-  } else if (wrongPicks.length === 3) {
-    $('.left-arm').removeClass('hidden')
-  } else if (wrongPicks.length === 4) {
-    $('.right-arm').removeClass('hidden')
-  } else if (wrongPicks.length === 5) {
-    $('.left-leg').removeClass('hidden')
-  } else if (wrongPicks.length === 6) {
-    $('.right-leg').removeClass('hidden')
-    getAnswer.text('get answer')
-    $('#result').text('YOU LOSE')
-    endHideElem.css('opacity', '0.5')
-    $('.eyes').animate({'opacity': '1'}, {'duration': 900})
-    $('.frown').animate({'opacity': '1'}, {'duration': 900})
-    $('#play-again').text('play again')
+  /* wrong answers to reveal hangman */
+  function showMan () {
+    if (wrongPicks.length === 1) {
+      $('.head').removeClass('hidden')
+    } else if (wrongPicks.length === 2) {
+      $('.torso').removeClass('hidden')
+    } else if (wrongPicks.length === 3) {
+      $('.left-arm').removeClass('hidden')
+    } else if (wrongPicks.length === 4) {
+      $('.right-arm').removeClass('hidden')
+    } else if (wrongPicks.length === 5) {
+      $('.left-leg').removeClass('hidden')
+    } else if (wrongPicks.length === 6) {
+      $('.right-leg').removeClass('hidden')
+      getAnswer.text('get answer')
+      $('#result').text('YOU LOSE')
+      endHideElem.css('opacity', '0.5')
+      $('.eyes').animate({'opacity': '1'}, {'duration': 900})
+      $('.frown').animate({'opacity': '1'}, {'duration': 900})
+      $('#play-again').text('play again')
+    }
   }
-}
 
-/* if lose, show answer */
-function showAnswer () {
-  wordCanvas.text(wordPlay)
-}
+  /* if lose, show answer */
+  function showAnswer () {
+    wordCanvas.text(wordPlay)
+  }
 }
 /* play again */
 $('#play-again').on('click', replay)
